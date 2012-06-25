8f01586627a4e3beddb57de28cbfacb6c005e5b1
MsJhane <jhane_alambra@yahoo.com>
2012-06-25 10:06:36 +0800

    Program2

--- OOP_Menu.py
+++ OOP_Menu.py
@@ -2,3 +2,16 @@ class MenuItem:
     def __init__(self):
         self.name=""
         self.price=0
+    def Show(self):
+        print("%s       %i "%(self.name,self.price))
+
+Spag=MenuItem()
+Spag.name="Spaghetti"
+Spag.price=95
+
+Coke=MenuItem()
+Coke.name="Coke"
+Coke.price=25
+
+Spag.Show()
+Coke.Show()
