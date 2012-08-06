8f9f3b19f43eeec52907559c1f128c6b834814a1
MsJhane <jhane_alambra@yahoo.com>
2012-08-06 10:36:00 +0800

    Tkinter: Message Box

---
+++ messagebox_tkinter5.py
@@ -0,0 +1,12 @@
+import tkinter
+
+root=tkinter.Tk()
+
+tkinter.messagebox.showinfo("title","message",icon=tkinter.messagebox.QUESTION)
+#icon=
+#tkinter.messagebox.ERROR
+#tkinter.messagebox.INFO
+#tkinter.messagebox.WARNING
+#tkinter.messagebox.QUESTION
+root.mainloop()
+root.destroy()
